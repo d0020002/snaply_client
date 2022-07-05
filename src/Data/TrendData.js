83e803ec5fce96cbf27f24a2075e0b8f169b6a5c
Deepak Singh Kushwaha <d0020002@gmail.com>
2022-07-05 18:14:04 +0530

    added homepage

---
+++ src/Data/TrendData.js
@@ -0,0 +1,27 @@
+export const TrendData= [
+    {
+      name: "Endless",
+      shares: 974,
+    },
+    {
+      name: "Leon",
+      shares: 80.5,
+    },
+    {
+      name: "DSA",
+      shares: 45.5,
+    },
+    {
+      name: "Reactjs",
+      shares: 42,
+    },
+    {
+      name: "Algorithms",
+      shares: 74.9,
+    },
+    {
+      name: "Ghazipur",
+      shares: 200,
+    },
+  ];
+  
